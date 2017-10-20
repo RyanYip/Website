99c795b3ed5f6ff53d00eb81cf8c8e51b6c642be
Ryan Yip <ryannyipis@gmail.com>
2017-10-19 19:39:06 -0700

    fixed spelling and grammar mistakes

--- html/gaming.php
+++ html/gaming.php
@@ -23,17 +23,17 @@
         <li>Create a Python script to pull data from the Steam Web API and store it into a MySQL database. The main problems I had with this step were:
           <ul>
             <li>Surrounding the variables with quotes so my insert statements would work correctly since the fields are "Strings". This threw me off since some fields needed it and others didn't. Eventually I figured out that the string fields required quotes, though I couldn't figure out the problem with the error message that was being output. (An example of this issue: <b><i>INSERT INTO games(name, image) VALUES "{0}", "{1}"</i></b>. Without the quotations my database would throw an error like <b><i>"Unknown column '[game title]' in 'field list'"</i></b> which was not very helpful at all since it <i>looked</i> like the game's title was surrounded in quotes already).</li>
-            <li>MySQL defaulting to latin-1 which threw errors inserting games with certain characters in the name. Figuring out how to convert the database to utf8 and ensuring further connections use utf8 was quite painful because it kept coming up everytime I wantd to access my database from a different script/file.</li>
+            <li>MySQL defaulting to latin-1 which threw errors inserting games with certain characters in the name. Figuring out how to convert the database to utf8 and ensuring further connections use utf8 was quite painful because it kept coming up everytime I wanted to access my database from a different script/file.</li>
           </ul>
         </li>
         <li>Set up a cron job to run the script daily at midnight to update the game list, playtimes, and recent games.
           <ul>
-            <li>Sometimes my script wouldn't run to update my game times. Turns out I had a deadlock with a differen't script trying to access my database. Staggering the scripts to run at slightly different times resolves the deadlock issue</li>
+            <li>Sometimes my script wouldn't run to update my game times. Turns out I had a deadlock with a different script trying to access my database. Staggering the scripts to run at slightly different times resolves the deadlock issue</li>
           </ul>
         </li>
         <li>Use PHP to extract the data from my database and insert it onto the page as HTML. The problem I encountered here was:
           <ul> 
-            <li>Using the thumbnail urls from steam were not secure (https) therefor making this page unsecure as well. I couldn't figure out why my page had lost its secure lock until I remembered the thumbnails. Solution to this was to download them and reference them internally instead of referencing the steam url for the images as I orginally was doing.</li>
+            <li>Using the thumbnail urls from steam were not secure (https) therefore making this page unsecure as well. I couldn't figure out why my page had lost its secure lock until I remembered the thumbnails. Solution to this was to download them and reference them internally instead of referencing the steam url for the images as I originally was doing.</li>
           </ul>
         </li> 
       </ul>
@@ -85,24 +85,18 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
     <td class="name">'.$name.'</td>
     <td class="playtime_forever" sorttable_customkey="'.$row["playtime_forever"].'">';
   if ($playtime_forever_hours > 0) {
-    echo $playtime_forever_hours.' hours';
+    echo $playtime_forever_hours.($playtime_forever_hours == 1 ? ' hour' : ' hours');
   } 
-  if ($playtime_forever_hours > 0 && $playtime_forever_minutes > 0) {
-    echo ' and '.$playtime_forever_minutes.' minutes';
-  }
-  if ($playtime_forever_hours == 0 && $playtime_forever_minutes > 0) {
-    echo $playtime_forever_minutes.' minutes';
+  if ($playtime_forever_minutes > 0) {
+    echo ($playtime_forever_hours > 0 ? ' and ' : '').$playtime_forever_minutes.($playtime_forever_minutes == 1 ? ' minute' : ' minutes');
   }
   echo '</td>
     <td class="playtime_2weeks" sorttable_customkey="'.$row["playtime_2weeks"].'">';
   if ($playtime_2weeks_hours > 0) {
-    echo $playtime_2weeks_hours.' hours';
+    echo $playtime_2weeks_hours.($playtime_2weeks_hours == 1 ? ' hour' : ' hours');
   } 
-  if ($playtime_2weeks_hours > 0 && $playtime_2weeks_minutes > 0) {
-    echo ' and '.$playtime_2weeks_minutes.' minutes';
-  }
-  if ($playtime_2weeks_hours == 0 && $playtime_2weeks_minutes > 0) {
-    echo $playtime_2weeks_minutes.' minutes';
+  if ($playtime_2weeks_minutes > 0) {
+    echo ($playtime_2weeks_hours > 0 ? ' and ' : '').$playtime_2weeks_minutes.($playtime_2weeks_minutes == 1 ? ' minute' : ' minutes');
   }
   echo '</td>
 </tr>
