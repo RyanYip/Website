d70497c3162824527853596fdb694cb738110ec8
Ryan Yip <ryannyipis@gmail.com>
2017-10-30 20:32:07 -0700

    Added pro2pro landing page

---
+++ html/pro2pro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Pro2Pro</title>
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+  </head>
+  <body>
+    <?php include('navbar.html')?>
+    <div class="wrapper">
+      <h1>Pro2Pro</h1>
+      <p>This is a temporary landing page for Pro2Pro which I am working on for fun with my friend, Luke. The web app can be found at <a href="https://www.ryany.org/pro2pro">www.ryany.org/pro2pro</a>.</br>
+For source code, please visit: <a href="https://www.github.com/lukezhang454/Pro2Pro">www.github.com/lukezhang454/Pro2Pro</a>.</p>
+
+    </div>
+    <script src="resources/js/jquery"></script>
+    <script src="resources/js/navbar.js"></script>
+  </body>
+</html>
--- html/resources/js/navbar.js
+++ html/resources/js/navbar.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
   var pathname = window.location.pathname.substring(1);
+  if (pathname === '') {
+    pathname = "index.php";
+  }
   $('#navbar li a[href="'+pathname+'"]').parent().attr('id', 'active');
 });
